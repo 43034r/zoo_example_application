62ac5e024bed683b4dd227e32461aa1e9d1584d1
43034r <91538748+43034r@users.noreply.github.com>
2021-09-28 14:35:43 +0300

    First commit
    First commit

---
+++ zoo/like_button.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
+
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
+  }
+
+  tick() {
+    this.setState(state => ({
+      seconds: state.seconds + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'Seconds: ',
+      this.state.seconds
+    );
+  }
+}
+
+ReactDOM.render(React.createElement(Timer, null), document.getElementById('timer-example'));
